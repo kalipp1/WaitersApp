11c80386b6b949c5e746bed65dec2e7e97d48884
Maks Birecki <maksymilian.birecki@gmail.com>
2024-10-14 15:36:24 +0200

    add layout for singleTable

--- src/components/pages/TablePage/TablePage.js
+++ src/components/pages/TablePage/TablePage.js
@@ -4,26 +4,39 @@ import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { getTableById } from '../../../redux/tablesRedux';
 import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const TablePage = () => {
     const { tableId } = useParams();
     const table = useSelector(state => getTableById(state, tableId));
     const statusOptions = ['Free', 'Reserved', 'Busy', 'Cleaning'];
 
+    if(!table) return <Navigate to={"/"} />
     return(
         <div>
-            <section>
+            <section className={styles.title}>
                 <h1>{table.name}</h1>
             </section>
             <section>
-                <p><b>Status:</b></p>
-                <select value={table.Status}>
-                    {statusOptions.map((status) => (
-                    <option key={status} value={status}>
-                    {status}
-                    </option>
-                    ))}
-                </select>
+                <div className={styles.status}>
+                    <p><b>Status:</b></p>
+                    <select value={table.Status}>
+                        {statusOptions.map((status) => (
+                        <option key={status} value={status}>
+                        {status}
+                        </option>
+                        ))}
+                    </select>
+                </div>
+                <div className={styles.people}>
+                <p><b>People:</b></p>
+                <input className={styles.peopleAmount} type='text' value={table.PeopleAmount} />/<input className={styles.peopleAmount} type='text' value={table.MaxPeopleAmount} />
+                </div>
+                <div className={styles.bill}>
+                    <p><b>Bill: <span className={styles.dollar}>$</span></b></p>
+                    <input className={styles.billInput} type='text' value={table.Bill} />
+                </div>
+                <button className={styles.updateBTN}>Update</button>
             </section>
         </div>
     );
